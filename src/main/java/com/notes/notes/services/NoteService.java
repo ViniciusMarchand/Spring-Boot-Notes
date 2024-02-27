e7128bdcc836b0662c92117e982cdfe5811630c6
Vinicius Marchand <vinicius.dasilva.marchand@gmail.com>
2024-02-26 22:08:48 -0300

    code cleaned

--- src/main/java/com/notes/notes/services/NoteService.java
+++ src/main/java/com/notes/notes/services/NoteService.java
@@ -3,16 +3,13 @@ package com.notes.notes.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.provider.HibernateUtils;
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Service;
 
 import com.notes.notes.models.Note;
 import com.notes.notes.repository.NoteRepository;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 
-import org.hibernate.Session;
 
 @Service
 public class NoteService {
@@ -23,4 +20,7 @@ public class NoteService {
     public List<Note> getAll() {
         return noteRepository.findAll();
     }
+
+
+    
 }
